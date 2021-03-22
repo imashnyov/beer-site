36ecea63d5a41faf600b3597ce395d85daae26f6
Yaroslav Martynenko <stikblacklabel@gmail.com>
2021-03-22 11:39:45 +0200

    add user comments and user marks models

--- backend/beer_app/admin.py
+++ backend/beer_app/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
-from .models import Beer
+from .models import Beer, UserComment, UserMark
 
-# Register your models here.
+
+class UserMarkInline(admin.StackedInline):
+    model = UserMark
+    # fields = ('owner__email', 'mark')
+    extra = 0
+
+
+class UserCommentInline(admin.StackedInline):
+    model = UserComment
+    # fields = ('owner__email', 'test')
+    extra = 0
+
+
 @admin.register(Beer)
 class PostAdmin(admin.ModelAdmin):
     list_filter = (
@@ -9,6 +21,11 @@ class PostAdmin(admin.ModelAdmin):
         'price',
         'created',
     )
+    list_display = (
+        'name',
+        'mark',
+        'created'
+    )
     search_fields = ('name',)
     date_hierarchy = 'created'
-
+    inlines = [UserMarkInline, UserCommentInline]
--- backend/beer_app/models.py
+++ backend/beer_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 
 def image_upload_path(instance, filename):
     '''
@@ -9,8 +10,19 @@ def image_upload_path(instance, filename):
     return f"images/{slugify(instance.name)}.png"
 
 
+class TimestampedModel(models.Model):
+    created = models.DateTimeField(
+        _('created at'),
+        auto_now_add=True,
+        blank=True,
+    )
+
+    class Meta:
+        abstract = True
+
+
 # Create your models here.
-class Beer(models.Model):
+class Beer(TimestampedModel):
     name = models.CharField(
         _('beer name'),
         max_length=255
@@ -18,12 +30,6 @@ class Beer(models.Model):
 
     description = models.TextField(
         _('beer description')
-    )
-
-    created = models.DateTimeField(
-        _('created at'),
-        auto_now_add=True,
-        blank=True,
     )
 
     mark = models.DecimalField(
@@ -46,12 +52,57 @@ class Beer(models.Model):
         upload_to = image_upload_path,
     )
 
-    #marks # user marks to this beer
-
-    #comments # user comments to this beer
+    #relations:
+    # user_marks
+    # user_comments
 
     class Meta:
         ordering = ['-created']
 
     def __str__(self) -> str:
         return self.name
+
+
+class UserMark(TimestampedModel):
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        help_text=_('owner of this mark')
+    )
+    beer = models.ForeignKey(
+        'Beer',
+        on_delete=models.CASCADE,
+        related_name='user_marks',
+        help_text=_(
+            'shows for which beer this mark is'
+        ),
+    )
+    mark = models.DecimalField(
+        _('beer mark'),
+        decimal_places=1,
+        max_digits=3,
+    )
+
+    class Meta:
+        ordering = ['-created']
+        unique_together = ['owner', 'beer']
+
+
+class UserComment(TimestampedModel):
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        help_text=_('owner of this mark')
+    )
+    beer = models.ForeignKey(
+        'Beer',
+        on_delete=models.CASCADE,
+        related_name='user_comments',
+        help_text=_(
+            'shows for which beer this comment is'
+        )
+    )
+    text = models.TextField()
+
+    class Meta:
+        ordering = ['-created']
