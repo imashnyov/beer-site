9c703465c940c1eb3a9a3e135b215685f4be9865
Yaroslav Martynenko <stikblacklabel@gmail.com>
2021-05-10 23:54:05 +0300

    add client

---
+++ client/src/views/Beers.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <li>
+        <ul v-for="beer in beers" :key="beer.id">
+            <div>
+                <h3>{{ beer.name }}</h3>
+                <img :src="beer.image" alt="beer_img">
+                <p>mark: {{ beer.mark }}</p>
+                <p>price: {{ beer.price }}</p>
+            </div>
+        </ul>
+    </li>
+    <button type="button" class="btn btn-primary">Load more</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      beers: [],
+    };
+  },
+  methods: {
+    getBeers() {
+      const path = `${process.env.VUE_APP_API_URL}/beers`;
+      axios.get(path)
+        .then((res) => {
+          this.beers = res.data.results;
+        })
+        .catch((error) => {
+          // eslint-отключение следующей строки
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getBeers();
+  },
+};
+</script>
